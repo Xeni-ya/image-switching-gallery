9aeda65cc46adc18d1f536595ac917abb4d7d327
Xeni-ya <budyuginaxeniya@gmail.com>
2024-08-16 19:15:32 +0300

    feat: page markup

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,2 +1,11 @@
 let showMessage = () => console.log('Я учу JavaScript!');
 showMessage();
+
+// Создай галерею переключения изображений. В ней будет две кнопки: вперёд и назад, которые должны переключать изображение (в галерее пока будет 2 картинки, но если можете больше, то делайте).
+
+const image = document.getElementById('elem-img');
+console.log(image);
+
+function addImage() {
+  image.src = './assets/img/zoe-gayah-jonker-G7kUPmzi80E.jpg';
+}
